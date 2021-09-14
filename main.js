157b08cc77f71e6249dff2caefea20a12d219e22
Davi Cintra <davi.cintra2002@gmail.com>
2021-09-14 03:57:22 -0300

    adding toggle feature to the menu

---
+++ main.js
@@ -0,0 +1,8 @@
+const nav = document.querySelector(".header1 nav");
+const toggle = document.querySelectorAll("nav .toggle");
+
+for (const element of toggle) {
+  element.addEventListener("click", () => {
+    nav.classList.toggle("show");
+  });
+}
